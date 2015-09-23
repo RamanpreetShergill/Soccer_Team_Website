5d76796d514ec025145797b36970ff08405906b3
RamanpreetShergill <preetraman228@gmail.com>
2015-09-23 14:02:39 -0400

    Project On Website

---
+++ js/jscript.js
@@ -0,0 +1,10 @@
+function setCookie() {
+			document.cookie = "sessionID=314592";
+		}
+		
+function stopRKey(evt) {
+			var evt = (evt) ? evt : ((event) ? event : null);
+			var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+			if ((evt.keyCode == 13) && (node.type=="text")) {return false;}
+		}
+		document.onkeypress = stopRKey;
